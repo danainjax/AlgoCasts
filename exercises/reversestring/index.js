5fbda49a639d9f5769f55f8fefb2430f62c6eb61
danainjax <danainjax@gmail.com>
2022-06-02 11:44:00 -0400

    reverse string with for loop

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -8,8 +8,18 @@
 
 //Approach 1- Built in methods, split the string into an array of individual characters, reverse the chars in the array, and then join them back into a string. Note: empty string "", not " ". A space and empty string give different results.
 
+// function reverse(str) {
+//   return str.split('').reverse().join('')
+// }
+
+// Approach 2- Use a for loop, create an empty string called reversed, for each char in the string, take the char and add it to the "start" of reversed, return the variable reversed.
+
 function reverse(str) {
-  return str.split('').reverse().join('')
+  let reversed = ''
+  for (let char of str) {
+    reversed = char + reversed
+  }
+  return reversed
 }
 
 module.exports = reverse
