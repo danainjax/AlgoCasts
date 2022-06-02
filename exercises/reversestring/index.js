b2e806a617403ea681acb1ab440621435a915562
danainjax <danainjax@gmail.com>
2022-06-02 08:59:24 -0400

    reverse string built in methods approach

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,10 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//Approach 1- Built in methods, split the string into an array of individual characters, reverse the chars in the array, and then join them back into a string. Note: empty string "", not " ". A space and empty string give different results.
 
-module.exports = reverse;
+function reverse(str) {
+  return str.split('').reverse().join('')
+}
+
+module.exports = reverse
